03141d5bdbbbc3779717729a3997aac76aa3bf7d
Ubaydulloh <ubaydulloh1000@mail.ru>
2023-11-11 00:06:16 +0500

    REF: logout

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,1 +1,6 @@
-urlpatterns = []
+from django.urls import path
+from django.contrib.auth.views import LogoutView
+
+urlpatterns = [
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
+]
